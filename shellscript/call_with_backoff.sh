0cf379704cabf8533682f957f50154391501f876
Diogo <davidiog@f45c899af327.ant.amazon.com>
2018-10-09 17:17:42 +1100

    Adding generic function to call an exponential backoff in shellscript

---
+++ shellscript/call_with_backoff.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+
+function with_backoff {
+  local max_attempts=${ATTEMPTS-5}
+  local timeout=${TIMEOUT-1}
+  local attempt=0
+  local exitCode=0
+
+  while [[ $attempt < $max_attempts ]]
+  do
+    "$@"
+    exitCode=$?
+
+    if [[ $exitCode == 0 ]]
+    then
+      break
+    fi
+
+    echo "Failure! Retrying in $timeout.." 1>&2
+    sleep $timeout
+    attempt=$(( attempt + 1 ))
+    timeout=$(( timeout * 2 ))
+  done
+
+  if [[ $exitCode != 0 ]]
+  then
+    echo "You've failed me for the last time! ($@)" 1>&2
+  fi
+
+  return $exitCode
+}
+
+COUNTER=1
+for i in `seq 1 2001`;
+do
+    echo $i
+    with_backoff ANY_AWS_COMMAND
+done
+
+exit 0
